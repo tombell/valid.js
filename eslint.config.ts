c422e0a602fceea1ac954026e57db86882a48b63
Tom Bell <tomb@tombell.dev>
2025-09-20 20:24:56 +0100

    Migrate to eslint

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/*"],
+  },
+
+  js.configs.recommended,
+
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+
+  prettierRecommended,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: [
+            "eslint.config.ts",
+            "prettier.config.js",
+            "src/*.test.ts",
+          ],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
